367e95c510543cfb21335fb5c310780ea9b5b491
Christian Elif Rivera Pulido <chrisriverapulido@hotmail.com>
2024-12-07 01:54:15 -0600

    Add files via upload

---
+++ act1/ChristianRivera_Actividad_A1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int edad;
+	cout << "ingrese un valor ";
+	cin >> edad;
+	if (edad > 17)
+	{
+		cout<<"ES MAYOR DE EDAD";
+	}
+	else{
+		cout << "ES MENOR DE EDAD";
+	}
+
+	return 0;
+	
+}
